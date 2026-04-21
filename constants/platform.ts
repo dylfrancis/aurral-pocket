f31b094b3b3820ab637d327339f3109897514953
dylfrancis <dylfrancis505@gmail.com>
2026-04-20 20:04:11 -0400

    refactor: add react-native-keyboard-controller

--- constants/platform.ts
+++ constants/platform.ts
@@ -2,6 +2,3 @@ import { Platform } from "react-native";
 
 export const IS_IOS = Platform.OS === "ios";
 export const IS_ANDROID = Platform.OS === "android";
-export const KEYBOARD_AVOIDING_BEHAVIOR = IS_IOS
-  ? "padding"
-  : ("height" as const);
